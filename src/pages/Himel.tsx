d151f1b19ea914b6862b1d79a01469f85e04b53f
enamul Hasan Himel <mhasan173013@bscse.uiu.ac.bd>
2025-02-20 15:04:35 +0600

    help desk update

--- src/pages/Himel.tsx
+++ src/pages/Himel.tsx
@@ -1,14 +1,14 @@
-import HelpDesk from "../section/helpDesk/HelpDesk"
-import HelpDeskHeader from "../section/helpDesk/HelpDeskHeader"
-
-
+import HelpDeskCategory from "../section/helpDesk/components/HelpDeskCategory";
+import HelpDesk from "../section/helpDesk/HelpDesk";
+import HelpDeskHeader from "../section/helpDesk/HelpDeskHeader";
 
 const Himel = () => {
-    return (
-        <div>
-            <HelpDeskHeader></HelpDeskHeader>
-        </div>
-    )
-}
+  return (
+    <div>
+      <HelpDeskHeader></HelpDeskHeader>
+      <HelpDeskCategory></HelpDeskCategory>
+    </div>
+  );
+};
 
-export default Himel
+export default Himel;
