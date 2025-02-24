bc5ce945fce406a2eddfdca26c6a5502faa79502
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-02-24 17:11:38 +0600

    add conseptual page and responsive tab

--- src/components/dashboard/Tabs.tsx
+++ src/components/dashboard/Tabs.tsx
@@ -1,41 +1,37 @@
-import { useState } from "react";
+import { cn } from "../../lib/utils";
 
-const tabs = () => {
-  const [firstActive, setFirstActive] = useState<boolean>(true);
-  firstActive;
+const tabs = ({
+  setTabs,
+  tabs,
+}: {
+  setTabs: React.Dispatch<React.SetStateAction<boolean>>;
+  tabs: boolean;
+}) => {
   return (
-    <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden flex-nowrap dark:bg-gray-100 dark:text-gray-800 mt-6">
-      <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800">
-        <button className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600 text-[#9553F9]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-4 h-4"
-          >
-            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-          </svg>
-          <span>Level 1 Course</span>
+    <div className="mt-8 w-full">
+      <div
+        className={`flex items-center gap-x-6 w-full
+        }`}
+      >
+        <button
+          onClick={() => setTabs(false)}
+          className={cn(
+            "text-white",
+            tabs === false &&
+              "relative px-4 py-2 text-purple-300 font-semibold border  border-purple-700 rounded-t-lg bg-[#0d0b17] after:absolute after:h-[1px] after:bg-purple-700  after:bottom-0"
+          )}
+        >
+          Level 1 Course
         </button>
-        <button className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg text-[#9553F9] dark:border-gray-600 dark:text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-4 h-4"
-          >
-            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-          </svg>
-          <span>Conseptual Class</span>
+        <button
+          onClick={() => setTabs(true)}
+          className={cn(
+            "text-white",
+            tabs === true &&
+              "relative px-4 py-2 text-purple-300 font-semibold border  border-purple-700 rounded-t-lg bg-[#0d0b17] after:absolute after:h-[1px] after:bg-purple-700  after:bottom-0"
+          )}
+        >
+          Conseptual Session
         </button>
       </div>
     </div>
