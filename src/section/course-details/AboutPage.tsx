4f7d6e3138f6f527527fbbff55229cb9e7639a83
eitty moni <eitty.t@gmail.com>
2025-03-01 17:05:58 +0600

    about page update

--- src/section/course-details/AboutPage.tsx
+++ src/section/course-details/AboutPage.tsx
@@ -2,6 +2,7 @@
 import CommonContainer from "../../common/CommonContainer";
 import CommonSpace from "../../common/CommonSpace";
 import Img from "../../assets/Jhankar-Mahbub.png"
+import { FaGreaterThan } from "react-icons/fa";
 
 
 interface TeamMember {
@@ -26,8 +27,17 @@ interface TeamCardProps {
 
 const TeamCard: React.FC<TeamCardProps> = ({ imgSrc, name, designation }) => {
   return (
-    <div className="bg-black border border-black shadow-lg rounded-lg overflow-hidden w-full sm:w-48 md:w-56 lg:w-64 m-4 transition-all duration-300 hover:scale-105 hover:border-purple-600 hover:shadow-lg hover:shadow-purple-500/50">
-    <img src={imgSrc} alt={name} className="w-full transition-all duration-300 hover:opacity-80" />
+    <div className="bg-black flex flex-col gap-4 items-center rounded-lg overflow-hidden w-full sm:w-48 md:w-56 lg:w-64 m-4 transition-all duration-300 hover:scale-105 group shadow-lg hover:shadow-purple-500">
+  <div className="relative">
+  <div className="">
+      <FaGreaterThan className="text-[#2D1B64]  group-hover:text-purple-500 text-[250px] z-0"/>
+    </div>
+    <div className="clip-custom-shape overflow-hidden absolute  top-[-15px] z-15">
+      <img src={imgSrc} alt={name} className="w-full transition-all duration-300 " />
+    </div>
+
+  </div>
+ 
     <div className="p-4">
       <h3 className="text-xl text-white font-semibold">{name}</h3>
       <p className="text-gray-500">{designation}</p>
@@ -43,7 +53,7 @@ const AboutPage: React.FC = () => {
       <CommonContainer>
         <CommonSpace>
           <div className="mx-auto p-8">
-            <h2 className="text-4xl font-bold text-center mb-8 text-white sm:text-3xl md:text-4xl">
+            <h2 className="md:text-7xl font-bold text-center mb-8 text-purple-300 text-3xl ">
               Meet our team_
             </h2>
             <div className="flex flex-wrap justify-center">
