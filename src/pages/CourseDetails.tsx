75e93db7abdda4341a5f5b6e4d3138c9cc3e3969
Eittymoni <eitty.t@gmail.com>
2025-03-12 11:30:14 +0600

    user condition added

--- src/pages/CourseDetails.tsx
+++ src/pages/CourseDetails.tsx
@@ -1,4 +1,5 @@
 
+import { useNavigate } from "react-router-dom";
 import Course from "../section/course-details/Course";
 import CoursedetailsHero from "../section/course-details/CoursedetailsHero";
 import CourseFooter from "../section/course-details/CourseFooter";
@@ -7,8 +8,24 @@ import CourseWork from "../section/course-details/CourseWork";
 import NextBatch from "../section/course-details/NextBatch";
 import Project from "../section/course-details/Project";
 import Specialty from "../section/course-details/Specialty";
+import { userStore } from "../store/UserStore";
+import { useEffect } from "react";
 
 const CourseDetails = () => {
+    const {user} = userStore();
+  
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/login"); 
+        }
+    }, [user, navigate]);
+
+    if (!user) {
+        return null;
+    }
+
   return (
     <div>
       <CourseNav />
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -3,8 +3,24 @@ import SuccessBody from "../section/helpDesk/components/SuccessBody";
 import CardList from "../section/helpDesk/components/card/CardList";
 import SuccessFooter from "../section/helpDesk/components/SuccessFooter";
 import CourseNav from "../section/course-details/CourseNav";
+import { userStore } from "../store/UserStore";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Success = () => {
+  const {user} = userStore();
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if (!user) {
+          navigate("/login"); 
+      }
+  }, [user, navigate]);
+
+  if (!user) {
+      return null;
+  }
   return (
     <div>
       <CourseNav />
